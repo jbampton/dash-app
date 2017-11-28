9acbad2274903e6ab706a979dffcce73cc1b62d9
John Bampton <jbampton@users.noreply.github.com>
2017-11-28 23:01:04 +1000

    Add comments, add ticker to dropdown

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
+"""Dash Charts Demo."""
+
+from datetime import datetime as dt
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 from pandas_datareader import data as web
 import pandas as pd
-from datetime import datetime as dt
+
 
 app = dash.Dash(__name__)
 server = app.server
 
 
 def populate():
+    """Get the 2 columns of data from the CSV file and return an array of labels and values."""
     csv_data = pd.read_csv('data/companylist.csv', usecols=['Symbol', 'Name'])
     csv_data = csv_data.sort_values(csv_data.columns[0])
     opts = []
-    for x in csv_data.to_dict('records'):
-        opts.append({'label': x['Name'], 'value': x['Symbol']})
+    for record in csv_data.to_dict('records'):
+        opts.append({'label': record['Name'] + ' ' + record['Symbol'], 'value': record['Symbol']})
     return opts
-
-
-# {'label': 'Apple', 'value': 'AAPL'},
-# {'label': 'Adobe', 'value': 'ADBE'},
-# {'label': 'Coke', 'value': 'COKE'},
-# {'label': 'Exxon', 'value': 'XOM'},
-# {'label': 'Microsoft', 'value': 'MSFT'},
-# {'label': 'Tesla', 'value': 'TSLA'}
 
 
 app.layout = html.Div([
     html.H1([
-        html.A('Dash', href="https://plot.ly/products/dash/", target="_blank", style={'float': 'left'}),
+        html.A('Dash', href="https://plot.ly/products/dash/",
+               target="_blank", style={'float': 'left'}),
         html.P(': A web application framework for Python.')
     ]),
     html.H3('NASDAQ Stock Tickers', style={'clear': 'both'}),
@@ -44,6 +41,7 @@ app.layout = html.Div([
 
 @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
+    """Update graph with new data of selected company."""
     df = web.DataReader(
         selected_dropdown_value, data_source='google',
         start=dt(2017, 1, 1), end=dt.now())
